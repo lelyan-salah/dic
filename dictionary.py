e92075e70c47d0b318b37491d1f8edb681d186a9
lelyan-salah <125682600+lelyan-salah@users.noreply.github.com>
2023-09-30 18:42:01 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,12 @@
 dict1 = {"Name": "Ahmed", "Age": 23}
 dict2 = {"City": "Gaza", "Gender": "Male"}
 
-concatenated_dict = {
-    "Name": "Ahmed",
-    "Age": dict1[23],
-    "City": "Gaza",
-    "Gender": dict2["Male"]
-}
+concatenated_dict = {}
+
+concatenated_dict.update(dict1)
+concatenated_dict.update(dict2)
+
+concatenated_dict["Name"] = "Ram"
+concatenated_dict["City"] = "Salem"
+
 print(concatenated_dict)
